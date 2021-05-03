c477ebf9c62d8a82899c418c94635316c27d07e0
nils-vanderwerf <n.vanderw.92@gmail.com>
2021-05-03 14:34:26 +1000

    All languages written

--- nested.rb
+++ nested.rb
@@ -15,7 +15,8 @@ def hopper
         :languages => ["C"]
       }
     }
-
+  
+   programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -36,7 +37,7 @@ def alan_kay_is_known_for
         :languages => ["C"]
       }
     }
-
+    programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +56,8 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
+
+    programmer_hash[:dennis_ritchie][:known_for] = programmer_hash[:dennis_ritchie][:languages][0]
 
 end
 
@@ -81,8 +84,13 @@ def adding_matz
         :languages => ["C"]
       }
     }
+    programmer_hash[:yukihiro_matsumoto] = {}
+    programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
+    programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
 
+    programmer_hash
 end
+
 
 def changing_alan
   # change what Alan Kay is :known_for to the value of the alans_new_info variable. 
@@ -104,6 +112,9 @@ def changing_alan
       }
     }
 
+    alans_new_info = "GUI"
+    programmer_hash[:alan_kay][:known_for] = alans_new_info
+    programmer_hash
 end
 
 def adding_to_dennis
@@ -126,4 +137,10 @@ def adding_to_dennis
       }
     }
 
+    programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+    puts programmer_hash
+    programmer_hash
+
 end
+
+adding_to_dennis
